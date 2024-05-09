5fc151aad3f5313264a09a361355956b08a81dff
Rob Marscher <rob@robmarscher.com>
2024-05-09 12:53:12 -0400

    Optimize release
    https://rustwasm.github.io/docs/book/reference/code-size.html

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-cargo clean && RUST_BACKTRACE=FULL cargo build --target wasm32-wasi --release
+cargo clean && cargo build --target wasm32-wasi --release
+mkdir target/wasm32-wasi/optimized
+wasm-opt -Os -o target/wasm32-wasi/optimized/argon2-wasi.wasm target/wasm32-wasi/release/argon2-wasi.wasm
+cp target/wasm32-wasi/optimized/argon2-wasi.wasm ./bin/argon2-wasi.wasm
